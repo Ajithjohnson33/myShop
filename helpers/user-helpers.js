790be2393ba977f6bac350981b96b0e331b56788
Ajith Johnson33 <ajithjohnson33@gmail.com>
2021-04-19 13:17:03 +0530

    product increment and decrement

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -37,27 +37,45 @@ module.exports = {
             }
         })
     },
+    
     addToCart:(proId,userId)=>{
+        let proObj={
+            item:objectId(proId),
+            quantity:1
+        }
         return new Promise(async(resolve,reject)=>{
             let userCart=await db.get().collection(collection.CART_COLLECTION).findOne({user:ObjectID(userId)})
             if(userCart){
+                let proExist=userCart.products.findIndex(product=> product.item==proId)
+                console.log(proExist);
+                if(proExist!=1){
+                    db.get().collection(collection.CART_COLLECTION)
+                    .updateOne({user:objectId(userId),'products.item':objectId(proId)},
+                    {
+                        $inc:{'products.$.quantity':1}
+                    }
+                    ).then(()=>{
+                        resolve()
+                    })
+                    
+                }else{
+                
                 db.get().collection(collection.CART_COLLECTION)
                 .updateOne({user:ObjectID(userId)},
                 {
                    
-                        $push:{products:ObjectID(proId)}
-                    
-
+                       $push:{products:proObj}
                 }
                
                 ).then((response)=>{
                     resolve()
                 })
+            }
 
             }else{
                 let cartObj={
-                    user:objectId(userID),
-                    products:[ObjectID(proID)]
+                    user:objectId(userId),
+                    products:[proObj]
                 }
                 db.get().collection(collection.CART_COLLECTION).insertOne(cartObj).then((response)=>{
                     resolve()
@@ -73,25 +91,92 @@ module.exports = {
                     $match:{user:ObjectID(userId)}
                 },
                 {
+                    $unwind:'$products'
+                },
+                {
+                    $project:{
+                        item:'$products.item',
+                        quantity:'$products.quantity'
+                    }
+                },
+                {
                     $lookup:{
                         from:collection.PRODUCT_COLLECTION,
-                        let:{prodList:'$products'},
-                        pipeline:[
-                            {
-                                $match:{
-                                    $expr:{
-                                        $in:['$_id',"$$prodList"]
-                                    }
-                                }
-                            }
-                        ],
-                        as:'cartItems'
+                        localField:'item',
+                        foreignField:'_id',
+                        as:'product'
 
+                    }
+                },
+                // {
+                //     $lookup:{
+                //         from:collection.PRODUCT_COLLECTION,
+                //         let:{prodList:'$products'},
+                //         pipeline:[
+                //             {
+                //                 $match:{
+                //                     $expr:{
+                //                         $in:['$_id',"$$prodList"]
+                //                     }
+                //                 }
+                //             }
+                //         ],
+                //         as:'cartItems'
+
+                //     }
+                // }
+
+                {
+                    $project:{
+                        item:1,quantity:1,product:{$arrayElemAt:['$product',0]}
                     }
                 }
                 
             ]).toArray()
-            resolve(cartItems[0].cartItems)
+            
+            resolve(cartItems)
         })
+    },
+    getCartCount:(userId)=>{
+        return new Promise(async(resolve,reject)=>{
+            let count=0
+            let cart=await db.get().collection(collection.CART_COLLECTION).findOne({user:objectId(userId)})
+            if(cart){
+                count=cart.products.length
+
+            }
+            resolve(count)
+        })
+    },
+
+
+    changeProductQuantity:(details)=>{
+        details.count=parseInt(details.count)
+        details.quantity=parseInt(details.quantity)
+        
+        return new Promise((resolve,reject)=>{
+            if(details.count==-1 && details.quantity==1){
+                db.get().collection(collection.CART_COLLECTION)
+                    .updateOne({_id:objectId(details.cart)},
+                     {
+                        $pull:{products:{item:objectId(details.product)}}
+                     }
+                    ).then((response)=>{
+                        resolve({removeProduct:true})
+                    })
+                       
+            
+            }else{        
+            db.get().collection(collection.CART_COLLECTION)
+                    .updateOne({_id:objectId(details.cart),'products.item':objectId(details.product)},
+                    {
+                        $inc:{'products.$.quantity':details.count}
+                    }
+                    ).then((response)=>{
+                        resolve(true)
+                    })
+                } 
+
+            })
     }
 }
--- routes/user.js
+++ routes/user.js
@@ -12,13 +12,17 @@ const verifyingLogin=(req,res,next)=>{
 }
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   let user=req.session.user
   console.log(user);
+  let cartCount=null
+  if(req.session.user){
+  cartCount= await userHelpers.getCartCount(req.session.user._id)
+}
 
   productHelpers.getAllProducts().then((products)=>{
     
-    res.render('user/view-products',{products,user});
+    res.render('user/view-products',{products,user,cartCount});
   })
 });
 router.get('/login',(req,res)=>{
@@ -66,14 +70,22 @@ router.get('/logout',(req,res)=>{
 router.get('/cart',verifyingLogin,async(req,res)=>{
   let products = await userHelpers.getCartProducts(req.session.user._id)
   console.log(products);
-  res.render('user/cart')
+  res.render('user/cart',{products,user:req.session.user})
 })
 
-router.get('/add-to-cart/:id',verifyingLogin,(req,res)=>{
+router.get('/add-to-cart/:id',(req,res)=>{
+  console.log("api call");
   userHelpers.addToCart(req.params.id,req.session.user._id).then(()=>{
-    res.redirect('/')
+    res.json({status:true})
   })
 })
 
+router.post('/change-product-quantity',(req,res,next)=>{
+  console.log(req.body);
+  userHelpers.changeProductQuantity(req.body).then((response)=>{
+    res.json(response)
+  })
+
+})
 
 module.exports = router;
